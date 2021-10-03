a141a694d517284f2cfd5ce9c8559ed66d75fb48
3b83 <bbatuhanbilecen@gmail.com>
2021-10-03 17:01:37 +0300

    Created binary event generation. Added masking to metrics.

---
+++ bin_events.py
@@ -0,0 +1,31 @@
+'''
+Saves accumulated events (count_data files, outputs of Spike-FlowNet: https://github.com/chan8972/Spike-FlowNet) as binary images.
+'''
+
+import numpy as np
+import cv2
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--countdatapath', type=str, help='Path of count_data folder')
+args = parser.parse_args()
+
+countdata_path_list = sorted(next(os.walk(args.countdatapath))[2]) #Does not include subfolders. All items in the directory must be npy files.
+
+dst = args.countdatapath + '/spikeimages'
+try:
+    os.makedirs(dst)
+except OSError:
+    pass
+
+for data in countdata_path_list:
+    count_data = np.load(args.countdatapath + '/' + data)
+    spike_img = np.zeros((count_data.shape[1],count_data.shape[2]))
+
+    for i in range(0,count_data.shape[3]):
+        spike_img += count_data[0,:,:,i]
+        spike_img += count_data[1,:,:,i]
+
+    ret, spike_img = cv2.threshold(spike_img, 0, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(dst + '/' + data[:-4] + '.png', spike_img)
--- metrics.py
+++ metrics.py
@@ -1,6 +1,8 @@
 '''
 Includes some metrics related to optical flow calculations.
-AEE (EPE) : average endpoint error (endpoint error)
+AEE (EPE) : average endpoint error
+
+AEE calculations can be masked with events, if desired.
 '''
 import os
 import argparse
@@ -13,7 +15,7 @@ parser.add_argument('--gtflowpath', type=str, help='Path of gt flow')
 parser.add_argument('--predflowpath', type=str, help='Path of pred flow')
 parser.add_argument('--ratio', type=int, help='Resize ratio for ground truth flow.')
 parser.add_argument('--crop', type=bool, default=True, help='True if flow files in gtflowpath need to be cropped in 1:1 (from center).')
-
+parser.add_argument('--maskpath', type=str, default=None, help='Path of event masks')
 args = parser.parse_args()
 
 def avg_endpoint_error(gt_flow, pred_flow):
@@ -21,8 +23,15 @@ def avg_endpoint_error(gt_flow, pred_flow):
     AEE = np.mean(EE)
     return AEE
 
+def avg_endpoint_error_masked(gt_flow, pred_flow, mask):
+    EE = np.linalg.norm(gt_flow - pred_flow, axis=-1)
+    EE = np.logical_and(EE, mask)
+    AEE = np.mean(EE)
+    return AEE
+
 gtflow_path_list = sorted(next(os.walk(args.gtflowpath))[2]) #Does not include subfolders. All items in the directory must be flo files.
 predflow_path_list = sorted(next(os.walk(args.predflowpath))[2])
+mask_path_list = sorted(next(os.walk(args.maskpath))[2])
 total_number_flow = len(gtflow_path_list)
 
 y = flowpy.flow_read(args.gtflowpath + gtflow_path_list[0]).shape[0]
@@ -33,7 +42,7 @@ AEE_sum = 0
 for i in range(0,total_number_flow-1):
     #Process ground truth flow (resize, cut from center)
     gt_flow = flowpy.flow_read(args.gtflowpath + gtflow_path_list[i])
-    
+
     f_y,f_x,_ = gt_flow.shape
     ratio = args.ratio
     crop_size = int(f_y/ratio) if y<=x else int(f_x/ratio)
@@ -69,6 +78,14 @@ for i in range(0,total_number_flow-1):
     else:
         pred_flow_resized = pred_flow
 
-    AEE_sum += avg_endpoint_error(gt_flow_resized, pred_flow_resized)
+    #If an event mask path is given, mask endpoint error inputs
+    if args.maskpath==None:
+        AEE_sum += avg_endpoint_error(gt_flow_resized, pred_flow_resized)
+    else:
+        mask = cv2.imread(args.maskpath + mask_path_list[i], cv2.IMREAD_GRAYSCALE)
+        #If mask and resized predicted flow dimensions do not match, resize mask
+        if (mask.shape[0] != pred_flow_resized.shape[0] or mask.shape[1] != pred_flow_resized.shape[1]):
+            mask = cv2.resize(mask, pred_flow_resized[...,0].shape)
+        AEE_sum += avg_endpoint_error_masked(gt_flow_resized, pred_flow_resized, mask)
     
 print('AEE: %f' %AEE_sum)
